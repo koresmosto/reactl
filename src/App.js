3d0b6327896e9c38fb8fdfba13dcce2c074f4999
koresmosto <koresmosto@gmail.com>
2021-11-17 00:10:59 +0200

    Redux simple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import HelloWorld from "./HelloWorld";
+import reducer from "./reducers";
+import {createStore} from "redux";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialState = {tech: "React "};
+const store = createStore(reducer, initialState);
+
+class App extends Component {
+  render() {
+    return <HelloWorld tech={store.getState().tech}/>
+  }
 }
 
 export default App;
